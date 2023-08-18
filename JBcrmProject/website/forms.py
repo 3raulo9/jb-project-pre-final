320a05344d3b92b9ac64e1e92f34c72df98c9e50
Raul <q4513545@gmail.com>
2023-08-18 21:03:32 +0300

    visually appealing forms

--- JBcrmProject/website/forms.py
+++ JBcrmProject/website/forms.py
@@ -1,16 +1,24 @@
-# Import required modules
 from django import forms
-from .models import Record
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Record
 
 
-# Custom SignUpForm class based on UserCreationForm
 class CustomSignUpForm(UserCreationForm):
-    # Adjust appearance of fields
-    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
-    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
-    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
+    email = forms.EmailField(
+        label="",
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'})
+    )
+    first_name = forms.CharField(
+        label="",
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'})
+    )
+    last_name = forms.CharField(
+        label="",
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'})
+    )
 
     class Meta:
         model = User
@@ -18,37 +26,88 @@ class CustomSignUpForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super(CustomSignUpForm, self).__init__(*args, **kwargs)
-
-        # Customize username field appearance
-        self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Username'})
         self.fields['username'].label = ''
         self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. Max 150 characters: letters, digits, @/./+/-/_ only.</small></span>'
 
-        # Adjust password1 field appearance
-        self.fields['password1'].widget.attrs['class'] = 'form-control'
-        self.fields['password1'].widget.attrs['placeholder'] = 'Enter Password'
-        self.fields['password1'].label = ''
-        self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Password should differ from personal info.</li><li>Minimum 8 characters.</li><li>Avoid common passwords.</li><li>Not entirely numeric.</li></ul>'
+        password_help_text = (
+            '<ul class="form-text text-muted small">'
+            '<li>Password should differ from personal info.</li>'
+            '<li>Minimum 8 characters.</li>'
+            '<li>Avoid common passwords.</li>'
+            '<li>Not entirely numeric.</li>'
+            '</ul>'
+        )
 
-        # Adapt password2 field appearance
-        self.fields['password2'].widget.attrs['class'] = 'form-control'
-        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
-        self.fields['password2'].label = ''
+        for field_name in ['password1', 'password2']:
+            self.fields[field_name].widget.attrs.update({'class': 'form-control', 'placeholder': f'Enter {field_name.replace("password", "Password")}'})
+            self.fields[field_name].label = ''
+            self.fields[field_name].help_text = password_help_text
+
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Re-enter the same password for verification.</small></span>'
 
-# Form for adding records
+
 class CustomAddRecordForm(forms.ModelForm):
-    # Customize field appearance and labels
-    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "First Name", "class": "form-control"}), label="")
-    last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Last Name", "class": "form-control"}), label="")
-    email = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Email", "class": "form-control"}), label="")
-    phone = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Phone", "class": "form-control"}), label="")
-    address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Address", "class": "form-control"}), label="")
-    city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "City", "class": "form-control"}), label="")
-    state = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "State", "class": "form-control"}), label="")
-    zipcode = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Zipcode", "class": "form-control"}), label="")
+    field_attrs = {
+        "placeholder": ["First Name", "Last Name", "Email", "Phone", "Address", "City", "State", "Zipcode"],
+        "class": "form-control"
+    }
+
+    first_name = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(attrs={"placeholder": field_attrs["placeholder"][0], "class": field_attrs["class"]}),
+        label=""
+    )
+    field_attrs["placeholder"].pop(0)
+
+    last_name = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(attrs={"placeholder": field_attrs["placeholder"][0], "class": field_attrs["class"]}),
+        label=""
+    )
+    field_attrs["placeholder"].pop(0)
+
+    email = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(attrs={"placeholder": field_attrs["placeholder"][0], "class": field_attrs["class"]}),
+        label=""
+    )
+    field_attrs["placeholder"].pop(0)
+
+    phone = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(attrs={"placeholder": field_attrs["placeholder"][0], "class": field_attrs["class"]}),
+        label=""
+    )
+    field_attrs["placeholder"].pop(0)
+
+    address = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(attrs={"placeholder": field_attrs["placeholder"][0], "class": field_attrs["class"]}),
+        label=""
+    )
+    field_attrs["placeholder"].pop(0)
+
+    city = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(attrs={"placeholder": field_attrs["placeholder"][0], "class": field_attrs["class"]}),
+        label=""
+    )
+    field_attrs["placeholder"].pop(0)
+
+    state = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(attrs={"placeholder": field_attrs["placeholder"][0], "class": field_attrs["class"]}),
+        label=""
+    )
+    field_attrs["placeholder"].pop(0)
+
+    zipcode = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(attrs={"placeholder": field_attrs["placeholder"][0], "class": field_attrs["class"]}),
+        label=""
+    )
 
     class Meta:
         model = Record
-        exclude = ("user",)  # Exclude 'user' field from the form
+        exclude = ("user",)
